3a35f2ea3ff2f4f6ef2b5cae58056a5ea5f346ad
YuliyB <yuliyboytsev2@yandex.ru>
2023-09-16 00:37:25 +0300

    Hibernate Implementation -изменил обьект UserDao на final

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private UserDao ud = new UserDaoHibernateImpl();
+    private final UserDao ud = new UserDaoHibernateImpl();
 
     @Override
     public void createUsersTable() {
